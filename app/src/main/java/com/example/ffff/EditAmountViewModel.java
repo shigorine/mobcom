881b9e1c3d1afc746cc9c1d58df0ed8eb9d51234
clarence <arenzmauricio12@gmail.com>
2023-10-21 08:14:28 +0800

    in progress edit

--- app/src/main/java/com/example/ffff/EditAmountViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.ffff;
-
-class EditAmountViewModel : ViewModel() {
-
-
-}
---
+++ app/src/main/java/com/example/ffff/model/ProfileModel.kt
@@ -0,0 +1,8 @@
+package com.example.ffff.model
+
+data class ProfileModel(
+    val id: Int? = null,
+    val email: String? = null,
+    var money: Double? = null,
+    val transactionId: String? = null
+)
